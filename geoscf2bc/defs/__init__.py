b3855393164e5a5dafab01ec8af1e238590cccfb
barronh <barronh@gmail.com>
2024-04-12 14:15:56 -0400

    fix glob

--- geoscf2bc/defs/__init__.py
+++ geoscf2bc/defs/__init__.py
@@ -7,7 +7,7 @@ from glob import glob
 
 
 defroot = os.path.realpath(os.path.dirname(__file__))
-defpaths = tuple(sorted(glob(defroot)))
+defpaths = tuple(sorted(glob(os.path.join(defroot, '*'))))
 metdefpath = [p for p in defpaths if p.endswith('geoscf_met.txt')][0]
 cb6r4defpath = [p for p in defpaths if p.endswith('geoscf_cb6r4.txt')][0]
 ae7defpath = [p for p in defpaths if p.endswith('geoscf_ae7.txt')][0]
