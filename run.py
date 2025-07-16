65d001e702fc283e46af7fd7ce0c765ae682f38d
Amir Souri <a.souri@nasa.gov>
2025-07-16 16:29:38 -0400

    fixing a bug in run.py

--- run.py
+++ run.py
@@ -13,10 +13,10 @@ def _daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + datetime.timedelta(n)
 
-def run_bcon(date1):
+def run_bcon(date1,date2):
     bcpaths = default(
       GDNAM='CONUS_8km', gdpath='./GRIDDESC',
-      SDATE=f'{date1}T00', EDATE=f'{date1}T00',m3path ='/discover/nobackup/asouri/MODELS/CMAQv5.5/data/mcip/CONUS_8km/METCRO3D_CONUS_8km_20230530.nc'
+      SDATE=f'{date1}T00', EDATE=f'{date2}T00',m3path ='/discover/nobackup/asouri/MODELS/CMAQv5.5/data/mcip/CONUS_8km/METCRO3D_CONUS_8km_20230530.nc'
       )
     return None
 
@@ -24,7 +24,7 @@ datarange = _daterange(datetime.date(2023, 6, 1), datetime.date(2023, 7, 1))
 datarange = list(datarange)
 output_files = []
 out = Parallel(n_jobs=12,verbose=10)(delayed(run_bcon)(
-           datarange[k].strftime('%Y%m%d')) for k in range(len(datarange)))
+           datarange[k].strftime('%Y%m%d'),(datarange[k] + datetime.timedelta(days=1)).strftime('%Y%m%d')) for k in range(len(datarange)))
 
 icpaths = default(
     GDNAM='CONUS_8km', gdpath='./GRIDDESC', ftype=1,
